8df69d18ee834ca394e0aa7243235eb102d215f2
data-bomb <data-bomb@users.noreply.github.com>
2023-07-06 20:49:14 -0700

    Update Updater.cs

--- UniversalUpdater/Updater.cs
+++ UniversalUpdater/Updater.cs
@@ -5,30 +5,8 @@ Copyright (C) 2023 by databomb
 * Description *
 Checks each DLL file in the Mods\ directory for the assemblyInfo 
 optional downloadLink URL. If a downloadLink URL is found then it 
-will try and check for an updater.json file at the URL
-
-https://yourdownloadlink.com/<mod namespace>/updater.json
-
-updater.json is of the format:
-{
-	"Version": "1.1.8",
-	"UpdateNotes": "Minor bug fixes",
-	"StoreBackup": true,
-	"Dependencies": {
-		"AdminExtension": {
-			"WebLocation": "https://raw.githubusercontent.com/data-bomb/Silica_ListenServer/main/Si_AdminExtension/bin/Si_AdminExtension.dll",
-			"LocalLocation": "MelonLoader\net6",
-			"ForceUpdate": false
-		}
-	}
-}
-
-if updater.json is found and the json Version is higher than the 
-assemblyInfo version then it will download the copy from 
-
-https://yourdownloadlink.com/<mod namespace>/bin/<mod namespace>.dll
-
-and place the updated version in the Mods\ directory
+will try and check for an updater.json file and download the latest
+version, if needed.
 
 * License *
 This program is free software: you can redistribute it and/or modify
