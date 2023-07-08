b30abe0f0bde98dd9957be298748c4b0c0c8d9e3
data-bomb <data-bomb@users.noreply.github.com>
2023-07-08 15:36:55 -0700

    Json Update and Minor Fixes
    - Adds Remote Path for binary in Json Updater format
    - Clears warnings and messages

--- UniversalUpdater/Updater.cs
+++ UniversalUpdater/Updater.cs
@@ -32,14 +32,14 @@ using System.Net;
 using UnityEngine;
 using UniversalUpdater;
 
-[assembly: MelonInfo(typeof(Updater), "Universal Mod Updater", "1.0.0", "databomb")]
+[assembly: MelonInfo(typeof(Updater), "Universal Mod Updater", "1.0.1", "databomb")]
 [assembly: MelonGame(null, null)]
 
 namespace UniversalUpdater
 {
     public class Updater : MelonPlugin
     {
-
+        
         public class UpdaterEntry
         {
             public String Version
@@ -47,7 +47,12 @@ namespace UniversalUpdater
                 get;
                 set;
             }
-            public String UpdateNotes
+            public String? RemotePath
+            {
+                get;
+                set;
+            }
+            public String? UpdateNotes
             {
                 get;
                 set;
@@ -66,12 +71,12 @@ namespace UniversalUpdater
 
         public class DependencyEntry
         {
-            public String WebLocation
+            public String? RemoteURL
             {
                 get;
                 set;
             }
-            public String LocalLocation
+            public String? LocalPath
             {
                 get;
                 set;
@@ -82,167 +87,178 @@ namespace UniversalUpdater
                 set;
             }
         }
+
         // iterate through mod files
         public override void OnPreInitialization() 
         {
-            string modsPath = MelonEnvironment.ModsDirectory;
-            DirectoryInfo modsDirectory = new DirectoryInfo(modsPath);
-            FileInfo[] modFiles = modsDirectory.GetFiles("*.dll");
-
-            HttpClient updaterClient = new HttpClient();
-
-            for (int i = 0; i < modFiles.Length; i++)
+            try
             {
-                FileInfo thisMod = modFiles[i];
-                String modFilePath = Path.Combine(modsPath, thisMod.Name);
-                byte[] byteBuffer = System.IO.File.ReadAllBytes(modFilePath);
-                string byteBufferAsString = System.Text.Encoding.UTF8.GetString(byteBuffer);
-                Int32 wrapOffset = byteBufferAsString.IndexOf("WrapNonExceptionThrows");
-                int typeOffset = 0;
+                string modsPath = MelonEnvironment.ModsDirectory;
+                DirectoryInfo modsDirectory = new(modsPath);
+                FileInfo[] modFiles = modsDirectory.GetFiles("*.dll");
 
-                if (wrapOffset != -1)
+                HttpClient updaterClient = new();
+                updaterClient.DefaultRequestHeaders.Add("User-Agent", "MelonUpdater");
+
+                for (int i = 0; i < modFiles.Length; i++)
                 {
-                    // align on first 'W'?
-                    for (int j = wrapOffset;  j < byteBuffer.Length; j++)
+                    FileInfo thisMod = modFiles[i];
+                    String modFilePath = Path.Combine(modsPath, thisMod.Name);
+                    byte[] byteBuffer = System.IO.File.ReadAllBytes(modFilePath);
+                    string byteBufferAsString = System.Text.Encoding.UTF8.GetString(byteBuffer);
+                    Int32 wrapOffset = byteBufferAsString.IndexOf("WrapNonExceptionThrows");
+                    int typeOffset = 0;
+
+                    if (wrapOffset != -1)
                     {
-                        // "W" 
-                        if (byteBuffer[j] == 87)
+                        // align on first 'W'?
+                        for (int j = wrapOffset; j < byteBuffer.Length; j++)
                         {
-                            wrapOffset = j;
-                            break;
+                            // "W" 
+                            if (byteBuffer[j] == 87)
+                            {
+                                wrapOffset = j;
+                                break;
+                            }
                         }
-                    }
-                    // add the length of the string plus offset to find start of assembly info
-                    typeOffset = wrapOffset + 36;
-                    // we're still off by one in many cases so let's try and find another reference in the backwards direction
-                    for (int j = typeOffset; j > 0; j--)
-                    {
-                        if (byteBuffer[j] == 0)
+                        // add the length of the string plus offset to find start of assembly info
+                        typeOffset = wrapOffset + 36;
+                        // we're still off by one in many cases so let's try and find another reference in the backwards direction
+                        for (int j = typeOffset; j > 0; j--)
                         {
-                            typeOffset = j+1;
-                            break;
+                            if (byteBuffer[j] == 0)
+                            {
+                                typeOffset = j + 1;
+                                break;
+                            }
                         }
-                    }
 
-                    // there are 4 required parameters and 1 optional parameter
-                    // https://melonwiki.xyz/#/modders/attributes
+                        // there are 4 required parameters and 1 optional parameter
+                        // https://melonwiki.xyz/#/modders/attributes
 
-                    // type (required)
-                    int size = byteBuffer[typeOffset];
-                    String type = System.Text.Encoding.UTF8.GetString(byteBuffer, typeOffset+1, size);
+                        // type (required)
+                        int size = byteBuffer[typeOffset];
+                        String type = System.Text.Encoding.UTF8.GetString(byteBuffer, typeOffset + 1, size);
 
-                    // name (required)
-                    int nameOffset = typeOffset + 1 + size;
-                    size = byteBuffer[nameOffset];
-                    String name = System.Text.Encoding.UTF8.GetString(byteBuffer, nameOffset + 1, size);
+                        // name (required)
+                        int nameOffset = typeOffset + 1 + size;
+                        size = byteBuffer[nameOffset];
+                        String name = System.Text.Encoding.UTF8.GetString(byteBuffer, nameOffset + 1, size);
 
-                    // version (required)
-                    int versionOffset = nameOffset + 1 + size;
-                    size = byteBuffer[versionOffset];
-                    String version = System.Text.Encoding.UTF8.GetString(byteBuffer, versionOffset + 1, size);
+                        // version (required)
+                        int versionOffset = nameOffset + 1 + size;
+                        size = byteBuffer[versionOffset];
+                        String version = System.Text.Encoding.UTF8.GetString(byteBuffer, versionOffset + 1, size);
 
-                    // author (required)
-                    int authorOffset = versionOffset + 1 + size;
-                    size = byteBuffer[authorOffset];
-                    String author = System.Text.Encoding.UTF8.GetString(byteBuffer, authorOffset + 1, size);
+                        // author (required)
+                        int authorOffset = versionOffset + 1 + size;
+                        size = byteBuffer[authorOffset];
+                        String author = System.Text.Encoding.UTF8.GetString(byteBuffer, authorOffset + 1, size);
 
-                    // downloadLink (optional)
-                    int downloadOffset = authorOffset + 1 + size;
-                    size = byteBuffer[downloadOffset];
+                        // downloadLink (optional)
+                        int downloadOffset = authorOffset + 1 + size;
+                        size = byteBuffer[downloadOffset];
 
-                    // check if optional downloadLink was included
-                    // 0xFF = NOT included
-                    if (size != 255)
-                    {
-                        String downloadLink = System.Text.Encoding.UTF8.GetString(byteBuffer, downloadOffset + 1, size);
-                        MelonLogger.Msg(type + " " + name + " " + version + " " + author + " " + downloadLink);
-
-                        // attempt to see if there is an update
-
-                        // build URL
-                        String modURL = type.Split('.')[0];
-                        String updaterText = "";
-
-                        if (downloadLink.StartsWith("http"))
+                        // check if optional downloadLink was included
+                        // 0xFF = NOT included
+                        if (size != 255)
                         {
-                            String updateURL = "";
-                            try
+                            String downloadLink = System.Text.Encoding.UTF8.GetString(byteBuffer, downloadOffset + 1, size);
+                            MelonLogger.Msg(type + " " + name + " " + version + " " + author + " " + downloadLink);
+
+                            // attempt to see if there is an update
+
+                            // build URL
+                            String modURL = type.Split('.')[0];
+                            String updaterText = "";
+
+                            if (downloadLink.StartsWith("http"))
                             {
-                                // check for GitHub and translate to raw URL
-                                if (downloadLink.StartsWith("https://github.com/"))
+                                String updateURL = "";
+                                try
                                 {
-                                    String githubAccount = downloadLink.Split('/')[3];
-                                    String githubRepo = downloadLink.Split('/')[4];
-                                    updateURL = "https://raw.githubusercontent.com/" + githubAccount + "/" + githubRepo + "/main/" + modURL + "/updater.json";
-                                }
-                                else
-                                {
-                                    updateURL = downloadLink + "/" + modURL + "/updater.json";
-                                }
-
-                                MelonLogger.Msg(updateURL);
-
-                                updaterText = updaterClient.GetStringAsync(updateURL).Result;
-                            }
-                            catch (Exception ex)
-                            {
-                                MelonLogger.Msg("Updater not found for " + thisMod.Name);
-                            }
-
-                            MelonLogger.Msg(updaterText);
-                            UpdaterEntry thisUpdater = JsonConvert.DeserializeObject<UpdaterEntry>(updaterText);
-
-                            if (thisUpdater == null)
-                            {
-                                MelonLogger.Msg("Skipping " + thisMod.Name + " due to json object corruption");
-                                continue;
-                            }
-
-                            // compare the two versions
-                            Version thisVersion = new Version(thisUpdater.Version);
-                            Version currentVersion = new Version(version);
-                            // is the updater version higher
-                            if (currentVersion.CompareTo(thisVersion) < 0)
-                            {
-                                MelonLogger.Msg("Updating " + thisMod.Name + "...");
-                                if (thisUpdater.UpdateNotes.Length > 0)
-                                {
-                                    MelonLogger.Msg(thisMod.Name + " Patch Notes- " + thisUpdater.UpdateNotes);
-                                }
-
-                                if (thisUpdater.StoreBackup)
-                                {
-                                    String backupDirectory = System.IO.Path.Combine(MelonEnvironment.ModsDirectory, @"backup\");
-                                    if (!System.IO.Directory.Exists(backupDirectory))
+                                    // check for GitHub and translate to raw URL
+                                    if (downloadLink.StartsWith("https://github.com/"))
                                     {
-                                        MelonLogger.Msg("Creating backup directory at: " + backupDirectory);
-                                        System.IO.Directory.CreateDirectory(backupDirectory);
+                                        String githubAccount = downloadLink.Split('/')[3];
+                                        String githubRepo = downloadLink.Split('/')[4];
+                                        updateURL = "https://raw.githubusercontent.com/" + githubAccount + "/" + githubRepo + "/main/" + modURL + "/updater.json";
+                                    }
+                                    else
+                                    {
+                                        updateURL = downloadLink + "/" + modURL + "/updater.json";
                                     }
 
-                                    MelonLogger.Msg("Moving " + thisMod.Name + " to backup directory");
-                                    String backupFilePath = Path.Combine(backupDirectory, thisMod.Name);
-                                    System.IO.File.Move(modFilePath, backupFilePath);
+                                    MelonLogger.Msg(updateURL);
+
+                                    updaterText = updaterClient.GetStringAsync(updateURL).Result;
+                                }
+                                catch
+                                {
+                                    MelonLogger.Msg("Updater not found for " + thisMod.Name);
                                 }
 
-                                // download new and replace
-                                String fileURL = updateURL.Remove(updateURL.Length - 13);
-                                fileURL = fileURL + "/bin/" + thisMod.Name;
-                                MelonLogger.Msg(fileURL);
-                                Stream downloadStream = updaterClient.GetStreamAsync(fileURL).Result;
-                                FileStream fileStream = new FileStream(modFilePath, FileMode.Create);
-                                downloadStream.CopyTo(fileStream);
+                                MelonLogger.Msg(updaterText);
+                                UpdaterEntry? thisUpdater = JsonConvert.DeserializeObject<UpdaterEntry>(updaterText);
 
-                                MelonLogger.Msg("Update for " + thisMod.Name + " complete.");
+                                if (thisUpdater == null)
+                                {
+                                    MelonLogger.Msg("Skipping " + thisMod.Name + " due to json object corruption");
+                                    continue;
+                                }
 
-                                // TODO: deal with dependencies
+                                // compare the two versions
+                                Version thisVersion = new(thisUpdater.Version);
+                                Version currentVersion = new(version);
+                                // is the updater version higher
+                                if (currentVersion.CompareTo(thisVersion) < 0)
+                                {
+                                    MelonLogger.Msg("Updating " + thisMod.Name + "...");
+                                    if (thisUpdater.UpdateNotes != null && thisUpdater.UpdateNotes.Length > 0)
+                                    {
+                                        MelonLogger.Msg(thisMod.Name + " Patch Notes- " + thisUpdater.UpdateNotes);
+                                    }
+
+                                    if (thisUpdater.StoreBackup)
+                                    {
+                                        String backupDirectory = System.IO.Path.Combine(MelonEnvironment.ModsDirectory, @"backup\");
+                                        if (!System.IO.Directory.Exists(backupDirectory))
+                                        {
+                                            MelonLogger.Msg("Creating backup directory at: " + backupDirectory);
+                                            System.IO.Directory.CreateDirectory(backupDirectory);
+                                        }
+
+                                        MelonLogger.Msg("Moving " + thisMod.Name + " to backup directory");
+                                        String backupFilePath = Path.Combine(backupDirectory, thisMod.Name);
+                                        System.IO.File.Move(modFilePath, backupFilePath);
+                                    }
+
+                                    // download new and replace
+                                    String fileURL = updateURL.Remove(updateURL.Length - 13);
+                                    fileURL = fileURL + "/bin/" + thisMod.Name;
+                                    MelonLogger.Msg(fileURL);
+                                    Stream downloadStream = updaterClient.GetStreamAsync(fileURL).Result;
+                                    FileStream fileStream = new(modFilePath, FileMode.Create);
+                                    downloadStream.CopyTo(fileStream);
+
+                                    MelonLogger.Msg("Update for " + thisMod.Name + " complete.");
+
+                                    // TODO: deal with dependencies
+                                }
                             }
-                        }
-                        else
-                        {
-                            MelonLogger.Msg("Download URL invalid for " + thisMod.Name);
+                            else
+                            {
+                                MelonLogger.Msg("Download URL invalid for " + thisMod.Name);
+                            }
                         }
                     }
                 }
+
+                updaterClient.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MelonLogger.Warning(ex.ToString());
             }
         }
     }
